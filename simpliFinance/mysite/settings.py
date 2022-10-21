4a1fe486326f8a391eeb02f3b65d41860a901f26
gillianbraun <111506532+gillianbraun@users.noreply.github.com>
2022-10-21 12:57:58 -0400

    Connecting to database

--- simpliFinance/mysite/settings.py
+++ simpliFinance/mysite/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django_cockroachdb',
+        'NAME': 'simpli-finance',
+        'USER': 'gillian',
+        'PASSWORD': 'e72wtL9fzgzNyUf5tOr8aQ',
+        'HOST': 'free-tier11.gcp-us-east1.cockroachlabs.cloud',
+        'PORT': '26257',
+        'OPTIONS': {
+            'sslmode': 'verify-full',
+            'options': '--cluster=simpli-finance-2462'
+        },
+    },
 }
 
 
