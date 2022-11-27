c1172d8c0bcb4a44a60053e6c3d277c808c6e429
Gillian B <111506532+gillianbraun@users.noreply.github.com>
2022-11-27 09:59:37 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -30,7 +30,7 @@ def landingPage(request):
 
 def getInfo(rooturl):
 #     url = "http://127.0.0.1:" + rooturl + "/product"
-    url = "127.0.0.1:10000/customer"
+    url = "http://127.0.0.1:10000/customer"
     api_call = requests.get(url, headers={})
     return json.loads(api_call.content)
 
